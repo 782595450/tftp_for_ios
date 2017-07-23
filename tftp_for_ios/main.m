540f5a34dfbcbbff43c54f7ea55ecf600ac12318
赖双全 <laishuangquan@detu.com>
2017-07-23 17:23:42 +0800

    Initial commit
    fisrst commit

---
+++ tftp_for_ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tftp_for_ios
+//
+//  Created by lsq on 2017/7/23.
+//  Copyright © 2017年 detu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
